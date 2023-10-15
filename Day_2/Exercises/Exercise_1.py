dac4dedddbac37061414ef97ac52a02c265fc500
PeterPan2151 <126306642+PeterPan2151@users.noreply.github.com>
2023-10-15 18:20:15 +0000

    Add files via upload

---
+++ Day_2/Exercises/Exercise_1.py
@@ -0,0 +1,20 @@
+# Exercise 1 From a two digit number sum up their digits and return the sum
+# number = input("Enter a two digit number: ")
+# num_one = int(number[0])
+# num_two = int(number[1])
+# sum = num_one + num_two
+# print(f"The sum is {sum}")
+
+# Exercise 2 BMI calculator Mvk. 1
+# height = float(input("Enter your height in m: "))
+# weight = int(input("Enter you weight in kg: "))
+# bmi = weight / height ** 2
+# print(f"Your BMI is {int(bmi)}")
+
+# Exercise 3 life in weeks
+age = int(input("What is your age? "))
+time_left = 90 - age
+days = time_left * 365
+weeks = time_left * 52
+months = time_left * 12
+print(f"You have {days} days, {weeks} weeks and {months} months left")
---
+++ Day_2/notes/note.py
@@ -0,0 +1,27 @@
+# Primitive data types
+# String
+# print("Hello"[4])
+
+# Integer
+# 12
+
+# Float
+# 3.1416
+
+# Boolean
+# True
+# False
+
+# Check type
+# x = 23
+# type(x)
+
+# math operators
+# 3 + 5
+# 3 - 5
+# 3 * 5
+# 3 / 5
+# 3 ** 5
+# 3 // 5
+# PEMDAS System (parenthesis, exponents, multiplication, division, addition, subtraction)
+# print(3 * (3 + 3) / 3 - 3)
