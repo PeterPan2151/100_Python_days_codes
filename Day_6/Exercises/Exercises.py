67414e459c140312e3b866845599bdeeb9d69bad
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2023-10-18 01:24:01 +0000

    Add files via upload

---
+++ Day_6/Exercises/Exercises.py
@@ -0,0 +1,56 @@
+# Exercise 1.Hurdle 1.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for i in range(6):
+#     move()
+#     jump()
+
+
+# Exercise 2. Hurdle2.
+
+# while not at_goal():
+#     move()
+#     jump()
+
+
+# Exercise 3. Hurdle 3.
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# Exercise 4. Hurdle 4.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
